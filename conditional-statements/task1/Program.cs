1b4f00cef5ea2b8246973e7b4c97f76a773e3311
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-12 13:36:42 +0300

    Updated task1

--- conditional-statements/task1/Program.cs
+++ conditional-statements/task1/Program.cs
@@ -21,17 +21,17 @@ namespace task1
                                    
             if (evaluatedNumber > 0)
             {
-                Console.WriteLine("number {0} is greater than 0", evaluatedNumber);
+                Console.WriteLine("number {0} is positive", evaluatedNumber);
             }
                       
             else if (evaluatedNumber < 0)
             {
-                Console.WriteLine("number {0} is less than 0", evaluatedNumber);
+                Console.WriteLine("number {0} is negative", evaluatedNumber);
             }
                         
             else
             {
-                Console.WriteLine("number {0} equals 0", evaluatedNumber);
+                Console.WriteLine("number {0} is 0", evaluatedNumber);
             } 
 
             //wait for user input
