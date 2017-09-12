0fc3adfeac0f52870a0dccef4330fea9bff104e4
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-12 19:46:16 +0300

    comitting task3

---
+++ conditional-statements/task3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt user
+            Console.Write("please enter a number");
+
+            // Read user input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if (evaluatedNumber > 0)
+            {
+
+                if (evaluatedNumber % 2 == 0)
+                {
+                    Console.WriteLine("number {0} is positive and even", evaluatedNumber);
+                }
+                else
+                    Console.WriteLine("number {0} is positive and odd", evaluatedNumber);
+            }
+            if (evaluatedNumber < 0)
+            {
+                if (evaluatedNumber % 2 == 0)
+                {
+                    Console.WriteLine("number {0} is negative and even", evaluatedNumber);
+                }
+                else
+                    Console.WriteLine("number {0} is negative and odd", evaluatedNumber);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
