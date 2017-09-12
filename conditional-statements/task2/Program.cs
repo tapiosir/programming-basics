d08b9a2c64a4382f4a4e859106b68406d42a4752
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-12 13:56:58 +0300

    task 2 commit

--- conditional-statements/task2/Program.cs
+++ conditional-statements/task2/Program.cs
@@ -6,7 +6,36 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // prompt user
+            Console.Write("please enter a number");
+
+            // Read user input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+           
+
+            if (evaluatedNumber % 2 == 0)
+            {
+                // number 0 is considered to be even
+                Console.WriteLine("number {0} is even", evaluatedNumber);
+            }
+
+            else
+            {
+                Console.WriteLine("number {0} is odd", evaluatedNumber);
+            }
+
+
+            // wait for user input
+            Console.ReadKey();
+
         }
     }
 }
