a1aac6603376b09e769925e24c380b6f366256ff
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-13 13:16:22 +0300

    committing unfinished task5

---
+++ conditional-statements/task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt user
+            Console.Write("enter age");
+            Console.WriteLine();
+
+            // Read user input
+            string userInput;
+            userInput = Console.ReadLine();
+            
+          
+            // Evaluate user input
+
+            int age;
+            int.TryParse(userInput, out age);
+
+            
+
+
+
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
