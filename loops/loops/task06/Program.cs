3f505c0af7df105d5e21779c5caa8b145f7b4f9c
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-19 13:12:56 +0300

    committing task6

---
+++ loops/loops/task06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            double j = 1;
+
+            do
+            {
+                j = Math.Sqrt(i);
+                Console.WriteLine("{0}\t {1}", i, j);
+
+            }
+            while (i++ < 10);
+            
+                
+            
+            Console.ReadKey();
+        }
+    }
+}
