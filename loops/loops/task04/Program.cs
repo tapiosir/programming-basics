35536832c60b25b073e05c5c9bff4979e1215c03
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-19 12:00:18 +0300

    committing task4

---
+++ loops/loops/task04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number");
+            Console.WriteLine();
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int num01;
+            int.TryParse(userInput, out num01);
+
+            int i = 1;
+            int j = 1;
+            int k = -1;
+            int l = -1;
+
+            if (num01 < 1)
+            {
+                while (l-- > num01)
+                {
+                    k = k + l;
+                }
+                Console.WriteLine("{0}", k);
+                
+            }
+
+            else
+            {
+                while (i++ < num01)
+                {
+                    j = j + i;
+                }
+                Console.WriteLine("{0}", j);
+            }
+            Console.ReadKey();
+        }
+    }
+}
