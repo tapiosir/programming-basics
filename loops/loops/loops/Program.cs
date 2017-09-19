c10db6559a6818fba267d95b1f84054f58d0436b
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-19 10:01:44 +0300

    committing loops

---
+++ loops/loops/loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number");
+            Console.WriteLine();
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int num01;
+            int.TryParse(userInput, out num01);
+
+            int j = 1;
+
+            if (num01 < 0)
+            {
+                Console.WriteLine("invalid");
+            }
+
+            else
+            {
+                for (int i = 1; i <= num01; i++)
+                {
+                    j = j * i;
+                }
+
+                Console.WriteLine("{0}", j);
+
+            }  
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ loops/loops/task02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
