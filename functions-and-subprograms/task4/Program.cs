21706b6021360835324c11cfbfdf00b76752f5cf
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-26 14:53:47 +0300

    .

--- functions-and-subprograms/task4/Program.cs
+++ functions-and-subprograms/task4/Program.cs
@@ -2,13 +2,39 @@
 
 namespace task4
 {
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
+            NumberCollector();
+            Console.ReadKey();
             
-           list<int>
         }
-         
+        public static void NumberCollector()
+        {
+            Console.WriteLine("enter 10");
+            string userInput;
+            userInput = Console.ReadLine();
+            int i;
+            int.TryParse(userInput, out i);
+            
+          
+            
+            List<int> list = new List<int>(); //https://www.dotnetperls.com/list
+
+            list.Add(i);
+            list.Add(i);
+
+            foreach (int prime in list)
+            {
+                Console.WriteLine(prime);
+            }
+
+
+
+
+        }
     }
 }
