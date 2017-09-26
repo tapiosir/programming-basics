5ac84216b75d4c79ffa3d4345e3175d336055fa0
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-26 09:39:35 +0300

    adding function and subprograms tasks

---
+++ functions-and-subprograms/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintStars();
+            Console.ReadKey();
+        }
+        public static void PrintStars()
+        {
+            Console.Write("please enter a number: ");
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int numberOfStars ;
+            int.TryParse(userInput, out numberOfStars);
+
+            if (numberOfStars < 0)
+            {
+                Console.WriteLine("{0} is not a positive number", numberOfStars);
+            }
+            else
+            {
+                for (int i = 0; i < numberOfStars; i++)
+                {
+                    Console.Write("*");
+                }
+            }
+
+
+
+
+
+        }
+
+    }
+}
