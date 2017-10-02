2816f010f872b550112bd3ee08fe91b992579e22
Tapiosir <tapio.sirvio11@gmail.com>
2017-10-02 13:23:20 +0300

    committing task05

---
+++ strings/task05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input text please.");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            string[] array = { "a", "e", "i", "o", "u", "y" };
+            int vowel = 0;
+
+            foreach (var c in array)
+            {
+                if (userInput.Contains(c))
+                {
+                    vowel++;
+                }
+            }
+            Console.WriteLine("word '{0}' contains {1} vowels",userInput,vowel);
+            Console.ReadKey();
+
+        }
+    }
+}
