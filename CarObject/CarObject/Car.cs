80ea7f1d97687791dfb67aab86881c30a1dcd471
Tapiosir <tapio.sirvio11@gmail.com>
2017-10-04 01:00:42 +0300

    committing finished CarObject.

--- CarObject/CarObject/Car.cs
+++ CarObject/CarObject/Car.cs
@@ -6,14 +6,31 @@ namespace CarObject
 {
     class Car
     {
-        string brand;
-        double speed;
+        public string Brand;
+        public double Speed;
 
-        public Car(string brand, double speed)
+        public Car(string brand)
         {
-          
+            Brand = brand;
+            Speed = 0;
         }
-        
+        public void Accelerate()
+        {
+            Speed = Speed + 12;
+        }
+        public void Accelerate(double value)
+        {
+            Speed = Speed + value;
+        }
+        public void Brake()
+        {
+            Speed = Speed * 0.9;
+        }
+
+        public override string ToString()
+        {
+            return $"brand: {Brand}, Speed: {Speed}";
+        }
 
 
     }
--- CarObject/CarObject/Program.cs
+++ CarObject/CarObject/Program.cs
@@ -8,8 +8,21 @@ namespace CarObject
         {
             Console.WriteLine("Hello World!");
 
-            Car car = new Car();
+            Car car = new Car("Ford");
+            Car car1 = new Car("Toyota");
+            Car car2 = new Car("Aixam");
+            Console.WriteLine("{0}\n{1}\n{2}\n",car,car1,car2);
 
+            car.Accelerate();
+            car1.Accelerate(34);
+            car2.Accelerate(87);
+
+            Console.WriteLine("{0}\n{1}\n{2}\n", car, car1, car2);
+
+            car.Brake();
+            car1.Brake();
+            car2.Brake();
+            Console.WriteLine("{0}\n{1}\n{2}\n", car, car1, car2);
             Console.ReadKey();
 
         }
